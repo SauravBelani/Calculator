c18fd36643502370e10c77b69c200226fd0606f1
SF-CPU-251\saurav <saurav.b@simformsolutions.com>
2022-02-04 16:52:01 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"The sum of the {x} and {y} is {x+y}");
+            Console.WriteLine($"The subtraction of the {x} and {y} is {x-y}");
+            Console.WriteLine($"The multiplication  of the {x} and {y} is {x*y}");
+            Console.WriteLine($"The division  of the {x} and {y} is {x/y}");
+        }
+    }
+}
